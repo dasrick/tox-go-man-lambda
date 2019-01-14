985baad56d238aec05e1518a4dfb0cefeeca0547
Enrico Hoffmann <dasrick@gmail.com>
2019-01-14 13:57:58 +0100

    append lambda unzip

--- cmd/unzip/main.go
+++ cmd/unzip/main.go
@@ -1,26 +1,24 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"os"
 )
 
 func main() {
-	//lambda.Start(HandleRequest)
-	HandleRequest()
+	lambda.Start(HandleRequest)
+	//HandleRequest()
 }
 
-//func HandleRequest(s3Event events.S3Event) () {
-//	for _, record := range s3Event.Records {
-//		s3 := record.S3
-//		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
-//	}
-//}
-
-func HandleRequest() {
-	//for _, record := range s3Event.Records {
-	//	s3 := record.S3
-	//	fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
-	//}
-
-	log.Print("hui buh")
+func HandleRequest(s3Event events.S3Event) {
+	pathIncoming := os.Getenv("S3_PATH_INCOMING")
+	fmt.Print(pathIncoming)
+	pathUncompressed := os.Getenv("S3_PATH_UNCOMPRESSED")
+	fmt.Print(pathUncompressed)
+	for _, record := range s3Event.Records {
+		s3 := record.S3
+		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
+	}
 }
