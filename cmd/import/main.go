a1e5a915fa9fcc3f3bd1bab7cea8c2536cd93ae6
Enrico Hoffmann <dasrick@gmail.com>
2019-01-17 20:36:26 +0100

    play play play

--- cmd/import/main.go
+++ cmd/import/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
@@ -9,7 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3iface"
-	"io/ioutil"
+	"io"
 	"log"
 	"os"
 )
@@ -57,26 +57,54 @@ func processS3Record(s3record events.S3EventRecord) error {
 		return fmt.Errorf("error in downloading %s from S3: %s\n", key, err)
 	}
 
-	body, err := ioutil.ReadAll(obj.Body)
-	if err != nil {
-		return fmt.Errorf("error in reading file %s: %s\n", key, err)
+	//requestInput := s3.GetObjectInput{
+	//	Bucket: aws.String(s3record.S3.Bucket.Name),
+	//	Key:    aws.String(s3record.S3.Object.Key),
+	//}
+	//buf := aws.NewWriteAtBuffer([]byte{})
+
+	//body, err := ioutil.ReadAll(obj.Body)
+	//if err != nil {
+	//	return fmt.Errorf("error in reading file %s: %s\n", key, err)
+	//}
+
+	r := csv.NewReader(bufio.NewReader(obj.Body))
+	r.Comma = ';'
+	r.Comment = '#'
+
+	// per line - generate person and PDF file
+	lineCount := 0
+	for {
+		record, err := r.Read()
+		// Stop at EOF.
+		if err == io.EOF {
+			break
+		}
+		// drop first AND second line because of header rows
+		if lineCount <= 1 {
+			lineCount += 1
+			continue
+		}
+		// map record data to person (good in case of structure change of CSV)
+		log.Println(record[0])
+		lineCount += 1
 	}
 
-	//reader := csv.NewReader(body)
+	////reader := csv.NewReader(body)
+	////record, err := reader.ReadAll()
+	////if err != nil {
+	////	fmt.Println("Error", err)
+	////}
+	//
+	//reader := csv.NewReader(bytes.NewBuffer(body))
 	//record, err := reader.ReadAll()
 	//if err != nil {
 	//	fmt.Println("Error", err)
 	//}
-
-	reader := csv.NewReader(bytes.NewBuffer(body))
-	record, err := reader.ReadAll()
-	if err != nil {
-		fmt.Println("Error", err)
-	}
-
-	for value := range record {
-		fmt.Println("", record[value])
-	}
+	//
+	//for value := range record {
+	//	fmt.Println("", record[value])
+	//}
 
 	return nil
 }
