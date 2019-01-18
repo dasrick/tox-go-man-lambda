ebd1d3cefed9cfa435c0bea49a56124e6e1875a9
Enrico Hoffmann <dasrick@gmail.com>
2019-01-18 10:53:10 +0100

    fix cll of lambda handler

--- cmd/import/main.go
+++ cmd/import/main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -25,28 +26,28 @@ type RowObject struct {
 }
 
 func main() {
-	//lambda.Start(HandleRequest)
+	lambda.Start(HandleRequest)
 
-	testRecord := events.S3EventRecord{
-		EventVersion: "2.0",
-		EventSource:  "aws:s3",
-		AWSRegion:    "eu-west-1",
-		//EventTime: "1970-01-01T00:00:00.000Z",
-		EventName: "ObjectCreated:Put",
-		S3: events.S3Entity{
-			Bucket: events.S3Bucket{
-				Name: "man-deveh-stash",
-				Arn:  "arn:aws:s3:::man-deveh-stash",
-			},
-			Object: events.S3Object{
-				Key: "uncompressed/Datei2_Haus_2018_short.csv",
-			},
-		},
-	}
-	err := HandleRequest(events.S3Event{Records: []events.S3EventRecord{testRecord}})
-	if err != nil {
-		log.Panicf("ein lustiger fehler:\n %s\n", err)
-	}
+	//testRecord := events.S3EventRecord{
+	//	EventVersion: "2.0",
+	//	EventSource:  "aws:s3",
+	//	AWSRegion:    "eu-west-1",
+	//	//EventTime: "1970-01-01T00:00:00.000Z",
+	//	EventName: "ObjectCreated:Put",
+	//	S3: events.S3Entity{
+	//		Bucket: events.S3Bucket{
+	//			Name: "man-deveh-stash",
+	//			Arn:  "arn:aws:s3:::man-deveh-stash",
+	//		},
+	//		Object: events.S3Object{
+	//			Key: "uncompressed/Datei2_Haus_2018_short.csv",
+	//		},
+	//	},
+	//}
+	//err := HandleRequest(events.S3Event{Records: []events.S3EventRecord{testRecord}})
+	//if err != nil {
+	//	log.Panicf("ein lustiger fehler:\n %s\n", err)
+	//}
 }
 
 func HandleRequest(s3Event events.S3Event) error {
