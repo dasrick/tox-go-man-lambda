37dfc9206130d713bc266b5e1ddf97f97f3b03c2
Enrico Hoffmann <dasrick@gmail.com>
2019-01-19 16:59:43 +0100

    play with sns event records

--- cmd/transformer/main.go
+++ cmd/transformer/main.go
@@ -12,31 +12,63 @@ func main() {
 	if !iamlocal {
 		lambda.Start(HandleRequest)
 	} else {
-		//testRecord := events.S3EventRecord{
-		//	EventVersion: "2.0",
-		//	EventSource:  "aws:s3",
-		//	AWSRegion:    "eu-west-1",
-		//	//EventTime: "1970-01-01T00:00:00.000Z",
-		//	EventName: "ObjectCreated:Put",
-		//	S3: events.S3Entity{
-		//		Bucket: events.S3Bucket{
-		//			Name: "man-deveh-stash",
-		//			Arn:  "arn:aws:s3:::man-deveh-stash",
-		//		},
-		//		Object: events.S3Object{
-		//			Key: "incoming/Datei2_Haus_2018.csv.gz",
-		//		},
-		//	},
-		//}
-		//err := HandleRequest(events.SNSEvent{Records: []events.S3EventRecord{testRecord}})
-		//if err != nil {
-		//	log.Panicf("ein lustiger fehler:\n %s\n", err)
-		//}
+
+		//	{
+		//		"Records": [
+		//	{
+		//		"EventSource": "aws:sns",
+		//		"EventVersion": "1.0",
+		//		"EventSubscriptionArn": "arn:aws:sns:eu-west-1:{{accountId}}:ExampleTopic",
+		//		"Sns": {
+		//			"Type": "Notification",
+		//			"MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
+		//			"TopicArn": "arn:aws:sns:eu-west-1:123456789012:ExampleTopic",
+		//			"Subject": "example subject",
+		//			"Message": "example message",
+		//			"Timestamp": "1970-01-01T00:00:00.000Z",
+		//			"SignatureVersion": "1",
+		//			"Signature": "EXAMPLE",
+		//			"SigningCertUrl": "EXAMPLE",
+		//			"UnsubscribeUrl": "EXAMPLE",
+		//			"MessageAttributes": {
+		//				"Test": {
+		//					"Type": "String",
+		//					"Value": "TestString"
+		//				},
+		//				"TestBinary": {
+		//					"Type": "Binary",
+		//					"Value": "TestBinary"
+		//				}
+		//			}
+		//		}
+		//	}
+		//]
+		//	}
+
+		testRecord := events.SNSEventRecord{
+			EventVersion: "1.0",
+			EventSource:  "aws:sns",
+			SNS: events.SNSEntity{
+				Type: "Notification",
+				//Message: {"HaID":"010530410005100006","StID":"0105304100051","HaAdrNStrasse":"Rehhagen","HaAdrNHausnr":"6","HaAdrNHausnrZu":"","HaAdrNPLZ":"23627","HaAdrNOTLName":"","HaAdrNOrtsname":"Groß Grönau","HaKooKGenau":"Adresspunkt"},
+			},
+		}
+		err := HandleRequest(events.SNSEvent{Records: []events.SNSEventRecord{testRecord}})
+		if err != nil {
+			log.Panicf("ein lustiger fehler:\n %s\n", err)
+		}
 	}
 }
 
-func HandleRequest(snsEvent events.SNSEvent) {
-	log.Print("===>>>")
-	log.Print(snsEvent)
-	log.Print("<<<===")
+func HandleRequest(snsEvent events.SNSEvent) error {
+	for _, snsRecord := range snsEvent.Records {
+		log.Println("TheRecord:", snsRecord)
+		log.Println("Subject:", snsRecord.SNS.Subject)
+		log.Println("Message:", snsRecord.SNS.Message)
+		//err := processS3Record(s3record)
+		//if err != nil {
+		//	return fmt.Errorf("error while processing %s from S3: %s\n", s3record.S3.Object.Key, err)
+		//}
+	}
+	return nil
 }
